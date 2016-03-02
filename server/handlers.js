0094d5d374cb99c65b9b86abfea7267e31d14b6e
Danielwhyte <danwhy@gmail.com>
2016-03-02 17:03:18 +0000

    adds live reloading with npm script dev

--- server/handlers.js
+++ server/handlers.js
@@ -1,12 +1,21 @@
+'use strict';
 const React = require('react');
 const ReactDomServer = require('react-dom/server');
+const Handlebars = require('handlebars');
+const fs = require('fs');
 const InitialPage = React.createFactory(require('../lib/components/initialPage.js'));
 
 const ItemFactory = React.createFactory(InitialPage)
 
 const handlers = {
   initialRender: function(req, res) {
-    res(ReactDomServer.renderToString(InitialPage({name: 'Daniel'})));
+    let InitialComponent = ReactDomServer.renderToString(InitialPage({name: 'Daniel'}));
+    let fileData = fs.readFileSync(__dirname + '/templates/index.html').toString();
+    let layoutTemplate = Handlebars.compile(fileData);
+
+    res(layoutTemplate({
+      content: InitialComponent
+    }));
   }
 };
 
